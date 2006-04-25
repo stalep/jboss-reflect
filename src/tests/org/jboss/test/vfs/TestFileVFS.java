d6308e03785ce584c747b3b87b736aefbdd0395a
starksm <starksm@84be2c1e-ba19-0410-b317-a758671a6fc1>
2006-04-25 00:47:55 +0000

    Correct and expand tests
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/container@44165 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/tests/org/jboss/test/vfs/TestFileVFS.java
+++ src/tests/org/jboss/test/vfs/TestFileVFS.java
@@ -6,31 +6,28 @@
 */
 package org.jboss.test.vfs;
 
-import java.net.URL;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
-import java.util.jar.JarFile;
-import java.util.jar.JarInputStream;
-import java.util.jar.Manifest;
-import java.util.jar.Attributes;
-import java.util.zip.ZipEntry;
-import java.util.Iterator;
-import java.util.HashMap;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.jar.Attributes;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+import java.util.zip.ZipInputStream;
 
-import junit.framework.TestCase;
-import org.jboss.vfs.VFSFactoryLocator;
+import org.jboss.test.BaseTestCase;
 import org.jboss.vfs.VFSFactory;
+import org.jboss.vfs.VFSFactoryLocator;
+import org.jboss.vfs.file.JarImpl;
+import org.jboss.vfs.file.NestedJarFromStream;
 import org.jboss.vfs.spi.ReadOnlyVFS;
 import org.jboss.vfs.spi.VirtualFile;
-import org.jboss.vfs.file.JarImpl;
-import org.jboss.test.AbstractTestCase;
-import org.jboss.test.BaseTestCase;
 
 /**
  * Tests of the VFS implementation
@@ -72,19 +69,39 @@ public class TestFileVFS extends BaseTestCase
       assertTrue(outerJar.getAbsolutePath()+" exists", outerJar.exists());
       JarFile jf = new JarFile(outerJar);
 
-      ZipEntry jar1 = jf.getEntry("jar1.jar");
-      JarInputStream jis1 = new JarInputStream(jf.getInputStream(jar1));
-      NestedJarFromStream njfs = new NestedJarFromStream(jis1);
+      JarEntry jar1 = jf.getJarEntry("jar1.jar");
+      URL jar1URL = new URL(outerJar.toURL(), "jar1.jar");
+      ZipInputStream jis1 = new ZipInputStream(jf.getInputStream(jar1));
+      NestedJarFromStream njfs = new NestedJarFromStream(jis1, jar1URL, jar1);
       NestedJarFromStream.JarEntryContents e1 = njfs.getEntry("org/jboss/test/vfs/support/jar1/ClassInJar1.class");
       assertNotNull(e1);
       log.info("org/jboss/test/vfs/support/CommonClass.class: "+e1);
+      NestedJarFromStream.JarEntryContents mfe1 = njfs.getEntry("META-INF/MANIFEST.MF");
+      assertNotNull("jar1!/META-INF/MANIFEST.MF", mfe1);
+      InputStream mfIS = mfe1.openStream();
+      Manifest mf = new Manifest(mfIS);
+      Attributes mainAttrs = mf.getMainAttributes();
+      String version = mainAttrs.getValue(Attributes.Name.SPECIFICATION_TITLE);
+      assertEquals("jar1", version);
+      mfIS.close();
+      njfs.close();
 
-      ZipEntry jar2 = jf.getEntry("jar2.jar");
-      JarInputStream jis2 = new JarInputStream(jf.getInputStream(jar2));
-      NestedJarFromStream njfs2 = new NestedJarFromStream(jis2);
+      JarEntry jar2 = jf.getJarEntry("jar2.jar");
+      URL jar2URL = new URL(outerJar.toURL(), "jar2.jar");
+      ZipInputStream jis2 = new ZipInputStream(jf.getInputStream(jar2));
+      NestedJarFromStream njfs2 = new NestedJarFromStream(jis2, jar2URL, jar2);
       NestedJarFromStream.JarEntryContents e2 = njfs2.getEntry("org/jboss/test/vfs/support/jar2/ClassInJar2.class");
       assertNotNull(e2);
       log.info("org/jboss/test/vfs/support/CommonClass.class: "+e2);
+      NestedJarFromStream.JarEntryContents mfe2 = njfs2.getEntry("META-INF/MANIFEST.MF");
+      assertNotNull("jar2!/META-INF/MANIFEST.MF", mfe2);
+      InputStream mf2IS = mfe2.openStream();
+      Manifest mf2 = new Manifest(mf2IS);
+      Attributes mainAttrs2 = mf2.getMainAttributes();
+      String version2 = mainAttrs2.getValue(Attributes.Name.SPECIFICATION_TITLE);
+      assertEquals("jar2", version2);
+      mf2IS.close();
+      njfs2.close();
       log.info("Ending heap usage: "+mem.getHeapMemoryUsage());
    }
 
@@ -155,9 +172,8 @@ public class TestFileVFS extends BaseTestCase
       VirtualFile[] contents = inner.getChildren();
       // META-INF/*, org/jboss/test/vfs/support/jar1/* at least
       assertTrue("jar1.jar children.length("+contents.length+") >= 2", contents.length >= 2);
-      for(int n = 0; n < contents.length; n ++)
+      for(VirtualFile vf : contents)
       {
-         VirtualFile vf = contents[n];
          log.info("  "+vf.getName());
       }
       ArrayList<String> searchCtxts = new ArrayList<String>();
@@ -166,58 +182,11 @@ public class TestFileVFS extends BaseTestCase
       InputStream mfIS = jar1MF.openStream();
       Manifest mf = new Manifest(mfIS);
       Attributes mainAttrs = mf.getMainAttributes();
-      String version = mainAttrs.getValue(Attributes.Name.SPECIFICATION_VERSION);
-      assertEquals("1.0.0.GA", version);
+      String version = mainAttrs.getValue(Attributes.Name.SPECIFICATION_TITLE);
+      assertEquals("jar1", version);
       mfIS.close();
    }
 
-   /**
-    * @todo need to create a seperate large vfs/jars traversal test
-    * @throws Exception
-    */
-   public void test403DefaultJarsSize()
-      throws Exception
-   {
-      MemoryMXBean mem = ManagementFactory.getMemoryMXBean();
-      log.info("Starting heap usage: "+mem.getHeapMemoryUsage());
-      File jboss403 = new File("/cvs/Releases/jboss-4.0.3");
-      File lib = new File(jboss403, "lib");
-      File defaultLib = new File(jboss403, "server/default/lib");
-
-      long size = 0;
-      File[] jars = lib.listFiles();
-      for(int n = 0; n < jars.length; n ++)
-      {
-         if( jars[n].isFile() == false )
-            continue;
-         FileInputStream fis = new FileInputStream(jars[n]);
-         JarInputStream jis = new JarInputStream(fis);
-         NestedJarFromStream njfs = new NestedJarFromStream(jis);
-         Iterator<NestedJarFromStream.JarEntryContents> iter = njfs.getEntries();
-         while( iter.hasNext() )
-         {
-            NestedJarFromStream.JarEntryContents jec = iter.next();
-            size += jec.getEntry().getSize();
-         }
-      }
-      jars = defaultLib.listFiles();
-      for(int n = 0; n < jars.length; n ++)
-      {
-         if( jars[n].isFile() == false )
-            continue;
-         FileInputStream fis = new FileInputStream(jars[n]);
-         JarInputStream jis = new JarInputStream(fis);
-         NestedJarFromStream njfs = new NestedJarFromStream(jis);
-         Iterator<NestedJarFromStream.JarEntryContents> iter = njfs.getEntries();
-         while( iter.hasNext() )
-         {
-            NestedJarFromStream.JarEntryContents jec = iter.next();
-            size += jec.getEntry().getSize();
-         }
-      }
-      log.info("Jar contents size: "+(size/(1024*1024))+"Mb");
-      log.info("Ending heap usage: "+mem.getHeapMemoryUsage());
-   }
    public void test403AllJarsSize()
       throws Exception
    {
@@ -230,33 +199,27 @@ public class TestFileVFS extends BaseTestCase
 
       long size = 0;
       File[] jars = lib.listFiles();
-      for(int n = 0; n < jars.length; n ++)
+      for(File jar : jars)
       {
-         if( jars[n].isFile() == false )
+         if( jar.isFile() == false )
             continue;
-         FileInputStream fis = new FileInputStream(jars[n]);
-         JarInputStream jis = new JarInputStream(fis);
-         NestedJarFromStream njfs = new NestedJarFromStream(jis);
-         Iterator<NestedJarFromStream.JarEntryContents> iter = njfs.getEntries();
-         while( iter.hasNext() )
+         JarImpl jf = new JarImpl(jar.getAbsolutePath());
+         VirtualFile[] children = jf.getChildren();
+         for(VirtualFile vf : children)
          {
-            NestedJarFromStream.JarEntryContents jec = iter.next();
-            size += jec.getEntry().getSize();
+            size += vf.getSize();
          }
       }
       jars = serverLib.listFiles();
-      for(int n = 0; n < jars.length; n ++)
+      for(File jar : jars)
       {
-         if( jars[n].isFile() == false )
+         if( jar.isFile() == false )
             continue;
-         FileInputStream fis = new FileInputStream(jars[n]);
-         JarInputStream jis = new JarInputStream(fis);
-         NestedJarFromStream njfs = new NestedJarFromStream(jis);
-         Iterator<NestedJarFromStream.JarEntryContents> iter = njfs.getEntries();
-         while( iter.hasNext() )
+         JarImpl jf = new JarImpl(jar.getAbsolutePath());
+         VirtualFile[] children = jf.getChildren();
+         for(VirtualFile vf : children)
          {
-            NestedJarFromStream.JarEntryContents jec = iter.next();
-            size += jec.getEntry().getSize();
+            size += vf.getSize();
          }
       }
       out.write("Jar contents size: "+(size/(1024*1024))+"Mb\n");
@@ -271,9 +234,8 @@ public class TestFileVFS extends BaseTestCase
       log.info("Size: "+ear.getSize());
       VirtualFile[] files = ear.getChildren();
       HashMap<String, VirtualFile> classes = new HashMap<String, VirtualFile>();
-      for(int n = 0; n < files.length; n ++)
+      for(VirtualFile vf : files)
       {
-         VirtualFile vf = files[n];
          log.info(vf);
          if( vf.isDirectory() )
             findClasses(vf, classes);
@@ -287,9 +249,8 @@ public class TestFileVFS extends BaseTestCase
                             HashMap<String, VirtualFile> classes) throws IOException
    {
       VirtualFile[] contents = jar.getChildren();
-      for(int n = 0; n < contents.length; n ++)
+      for(VirtualFile vf : contents)
       {
-         VirtualFile vf = contents[n];
          if( vf.isDirectory() )
             findClasses(vf, classes);
          if( vf.getName().endsWith(".class") )
