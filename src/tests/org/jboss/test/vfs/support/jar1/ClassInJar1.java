98123fedcd983b93c004ba00d08985935f98b611
scott.stark@jboss.org <scott.stark@jboss.org@84be2c1e-ba19-0410-b317-a758671a6fc1>
2006-08-07 22:11:01 +0000

    Add an inner class
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/microcontainer/trunk/container@55388 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/tests/org/jboss/test/vfs/support/jar1/ClassInJar1.java
+++ src/tests/org/jboss/test/vfs/support/jar1/ClassInJar1.java
@@ -7,4 +7,8 @@ package org.jboss.test.vfs.support.jar1;
 public class ClassInJar1
 {
    public static final String VERSION="1.0.0";
+   static class InnerClass
+   {
+      public static final String VERSION="1.0.0i";      
+   }
 }
