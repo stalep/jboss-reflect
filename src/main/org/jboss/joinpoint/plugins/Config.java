43f9e85c0147a0fd7d5ac2998067a3986fe69fc5
alesj <alesj@84be2c1e-ba19-0410-b317-a758671a6fc1>
2006-08-29 13:19:16 +0000

    Added / used some isStatic and isPublic limitations to Configurator.
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/microcontainer/trunk/container@56399 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/org/jboss/joinpoint/plugins/Config.java
+++ src/main/org/jboss/joinpoint/plugins/Config.java
@@ -38,7 +38,7 @@ import org.jboss.reflect.spi.TypeInfo;
 
 /**
  * Config utilities.
- * 
+ *
  * @author <a href="adrian@jboss.com">Adrian Brock</a>
  * @version $Revision$
  */
@@ -46,16 +46,16 @@ public class Config
 {
    /** The log */
    protected static final Logger log = Logger.getLogger(Config.class);
-   
+
    /** No parameter types */
    private static final String[] NO_PARAMS_TYPES = new String[0];
-   
+
    /** No parameters */
    private static final Object[] NO_PARAMS = new Object[0];
-   
+
    /**
     * Instantiate an object
-    * 
+    *
     * @param jpf the join point factory
     * @param paramTypes the parameter types
     * @param params the parameters
@@ -85,7 +85,7 @@ public class Config
 
    /**
     * Unconfigure a field
-    * 
+    *
     * @param object the object to unconfigure
     * @param jpf the join point factory
     * @param name the name of the field
@@ -99,7 +99,7 @@ public class Config
 
    /**
     * Invoke a method
-    * 
+    *
     * @param object the object to invoke
     * @param jpf the join point factory
     * @param name the name of the method
@@ -113,10 +113,10 @@ public class Config
       MethodJoinpoint joinpoint = getMethodJoinpoint(object, jpf, name, paramTypes, params);
       return joinpoint.dispatch();
    }
-   
+
    /**
     * Get a constructor Joinpoint
-    * 
+    *
     * @param jpf the join point factory
     * @return the Joinpoint
     * @throws Throwable for any error
@@ -125,10 +125,10 @@ public class Config
    {
       return getConstructorJoinpoint(jpf, null, null);
    }
-   
+
    /**
     * Get a constructor Joinpoint
-    * 
+    *
     * @param jpf the join point factory
     * @param paramTypes the parameter types
     * @param params the parameters
@@ -142,7 +142,7 @@ public class Config
 
       if (params == null)
          params = NO_PARAMS;
-      
+
       boolean trace = log.isTraceEnabled();
       if (trace)
          log.trace("Get constructor Joinpoint jpf=" + jpf + " paramTypes=" + Arrays.asList(paramTypes) + " params=" + Arrays.asList(params));
@@ -167,7 +167,7 @@ public class Config
       boolean trace = log.isTraceEnabled();
       if (trace)
          log.trace("Get field get Joinpoint jpf=" + jpf + " target=" + object + " name=" + name);
-      
+
       FieldInfo fieldInfo = findFieldInfo(jpf.getClassInfo(), name);
       FieldGetJoinpoint joinpoint = jpf.getFieldGetJoinpoint(fieldInfo);
       joinpoint.setTarget(object);
@@ -176,7 +176,7 @@ public class Config
 
    /**
     * Get a field set joinpoint
-    * 
+    *
     * @param object the object to configure
     * @param jpf the join point factory
     * @param name the name of the field
@@ -189,7 +189,7 @@ public class Config
       boolean trace = log.isTraceEnabled();
       if (trace)
          log.trace("Get field set Joinpoint jpf=" + jpf + " target=" + object + " name=" + name + " value=" + value);
-      
+
       FieldInfo fieldInfo = findFieldInfo(jpf.getClassInfo(), name);
       FieldSetJoinpoint joinpoint = jpf.getFieldSetJoinpoint(fieldInfo);
       joinpoint.setTarget(object);
@@ -199,7 +199,7 @@ public class Config
 
    /**
     * Get a method joinpoint
-    * 
+    *
     * @param object the object to invoke
     * @param jpf the join point factory
     * @param name the name of the method
@@ -225,10 +225,10 @@ public class Config
       joinpoint.setArguments(params);
       return joinpoint;
    }
-   
+
    /**
     * Find constructor info
-    * 
+    *
     * @param classInfo the class info
     * @param paramTypes the parameter types
     * @return the constructor info
@@ -248,10 +248,10 @@ public class Config
       }
       throw new JoinpointException("Constructor not found " + classInfo.getName() + Arrays.asList(paramTypes) + " no constructors");
    }
-   
+
    /**
     * Find field info
-    * 
+    *
     * @param classInfo the class info
     * @param name the field name
     * @return the field info
@@ -269,10 +269,10 @@ public class Config
       }
       throw new JoinpointException("Field not found '" + name + "' for class " + classInfo.getName());
    }
-   
+
    /**
     * Find field info
-    * 
+    *
     * @param fieldInfo the field info
     * @param name the field name
     * @return the field info or null if not found
@@ -290,10 +290,10 @@ public class Config
       }
       return null;
    }
-   
+
    /**
     * Find method info
-    * 
+    *
     * @param classInfo the class info
     * @param name the method name
     * @param paramTypes the parameter types
@@ -302,36 +302,57 @@ public class Config
     */
    public static MethodInfo findMethodInfo(ClassInfo classInfo, String name, String[] paramTypes) throws JoinpointException
    {
+      return findMethodInfo(classInfo, name, paramTypes, false, true);
+   }
+
+   /**
+    * Find method info
+    *
+    * @param classInfo the class info
+    * @param name the method name
+    * @param paramTypes the parameter types
+    * @param isStatic must the method be static
+    * @param isPublic must the method be public
+    * @return the method info
+    * @throws JoinpointException when no such method
+    */
+   public static MethodInfo findMethodInfo(ClassInfo classInfo, String name, String[] paramTypes, boolean isStatic, boolean isPublic) throws JoinpointException
+   {
       if (paramTypes == null)
          paramTypes = NO_PARAMS_TYPES;
-      
+
       ClassInfo current = classInfo;
       while (current != null)
       {
-         MethodInfo result = locateMethodInfo(current, name, paramTypes);
+         MethodInfo result = locateMethodInfo(current, name, paramTypes, isStatic, isPublic);
          if (result != null)
             return result;
          current = current.getSuperclass();
       }
       throw new JoinpointException("Method not found " + name + Arrays.asList(paramTypes) + " for class " + classInfo.getName());
    }
-   
+
    /**
     * Find method info
-    * 
+    *
     * @param classInfo the class info
     * @param name the method name
     * @param paramTypes the parameter types
+    * @param isStatic must the method be static
+    * @param isPublic must the method be public
     * @return the method info or null if not found
     */
-   private static MethodInfo locateMethodInfo(ClassInfo classInfo, String name, String[] paramTypes)
+   private static MethodInfo locateMethodInfo(ClassInfo classInfo, String name, String[] paramTypes, boolean isStatic, boolean isPublic)
    {
       MethodInfo[] methods = classInfo.getDeclaredMethods();
       if (methods != null)
       {
          for (int i = 0; i < methods.length; ++i)
          {
-            if (name.equals(methods[i].getName()) && equals(paramTypes, methods[i].getParameterTypes()))
+            if (name.equals(methods[i].getName()) &&
+                equals(paramTypes, methods[i].getParameterTypes()) &&
+                methods[i].isStatic() == isStatic &&
+                methods[i].isPublic() == isPublic)
                return methods[i];
          }
       }
@@ -340,7 +361,7 @@ public class Config
 
    /**
     * Test whether type names are equal to type infos
-    * 
+    *
     * @param typeNames the type names
     * @param typeInfos the type infos
     * @return true when they are equal
