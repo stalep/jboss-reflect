029238e38eeafc89d18d6eabcd5bfb42b2d33f9f
kabir.khan@jboss.com <kabir.khan@jboss.com@84be2c1e-ba19-0410-b317-a758671a6fc1>
2006-09-21 12:03:20 +0000

    [JBAOP-286] Weaken dependencies on unreleased MC 2.0
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/microcontainer/trunk/container@57049 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/org/jboss/annotation/factory/ast/ASTChar.java
+++ src/main/org/jboss/annotation/factory/ast/ASTChar.java
@@ -20,7 +20,7 @@
   * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
   */
 
-package org.jboss.annotation.factory.ast;
+package org.jboss.aop.annotation.factory.duplicate.ast;
 
 public class ASTChar extends ASTMemberValue
 {
